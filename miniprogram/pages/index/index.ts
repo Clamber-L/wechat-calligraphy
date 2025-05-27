b28699cab464ebf0750c33b38e0b79798781a1b3
Carlos <61411632+CarlosLees@users.noreply.github.com>
2025-05-27 16:54:35 +0800

    fix ui

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -12,14 +12,7 @@ interface User {
 Component({
     data: {
         userList: [{}], // 用户头像列表
-        scrollTarget: '',
-        scrollIndex: 0,
-        scrollTimer: 0,
-
-        orderList: [{}],
-        orderAnimation: null,
-        orderY: 0,
-        groupSize: 5 // 每组显示5条
+        orderList: [{}]
     },
     methods: {
         onLoad: function () {
@@ -30,57 +23,42 @@ Component({
 
             this.setData({ userList: users })
 
-            this.data.scrollIndex = 0
-            this.startAutoScroll()
-
             const orders = Array.from({ length: 20 }, (_, i) => ({
                 name: `用户${i + 1}`,
                 phone: `18****${1000 + i}`,
                 price: '198.00'
             }))
 
-            this.setData({ orderList: orders })
-
-            this.data.orderIndex = 0
-            this.startGroupedScroll()
+            this.setData({
+                orderList: orders
+            })
         },
-        startAutoScroll() {
-            const interval = 2000 // 每2秒滚动
-            this.data.scrollTimer = setInterval(() => {
-                const total = this.data.userList.length
-                const itemsPerRow = 4
-                const rows = Math.ceil(total / itemsPerRow)
 
-                this.data.scrollIndex = (this.data.scrollIndex + 1) % rows
-
-                const targetId = `user-${this.data.scrollIndex * itemsPerRow}`
-                this.setData({
-                    scrollTarget: targetId
-                })
-            }, interval)
-        },
-        startGroupedScroll() {
-            const groupHeight = 40 * this.data.groupSize
-            const totalGroups = Math.ceil(this.data.orderList.length / this.data.groupSize)
-
-            this.data.orderTimer = setInterval(() => {
-                this.data.orderIndex = (this.data.orderIndex + 1) % totalGroups
-                const orderY = -this.data.orderIndex * groupHeight
-
-                const animation = wx.createAnimation({
-                    duration: 500,
-                    timingFunction: 'ease'
-                })
-                animation.translateY(orderY).step()
-
-                this.setData({
-                    orderAnimation: animation.export(),
-                    orderY
-                })
-            }, 3000) // 每3秒滚动一组
-        },
         onUnload() {
             clearInterval(this.data.scrollTimer), clearInterval(this.data.orderTimer)
+        },
+
+        /**
+         * 用户点击右上角分享
+         */
+        onShareAppMessage(obj: any) {
+            console.log(obj)
+            return {
+                title: '赶紧买课，早买早享受，晚买享折扣',
+                imageUrl: '../../assets/avatar.png',
+                path: '/pages/index/index'
+            }
+        },
+
+        /**
+         * 分享到朋友圈
+         */
+        onShareTimeline() {
+            return {
+                title: '帮我砍一刀',
+                query: 'id=1',
+                imageUrl: '../../assets/avatar.png'
+            }
         }
     }
 })
